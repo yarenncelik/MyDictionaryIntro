b609d6899543a108cbfbb0a0c8230edda5d8e02f
Yaren Çelik <yarenn.celik9@gmail.com>
2021-04-15 23:41:58 +0300

    Add project files.

---
+++ MyDictionaryIntro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionaryIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> student = new MyDictionary<int,string>();
+
+            student.Add(152, "Yaren Çelik");
+            student.Add(153, "Kerem Kara");
+            student.Add(154, "Özlem Duru");
+
+            student.ToList();
+        }
+    }
+}
